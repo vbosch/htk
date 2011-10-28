b466ff523ff4409afbf7fe6744849a53a69e766f
vbosch <vbosch@gmail.com>
2011-10-28 13:04:32 +0200

    Created initial version of Configuration Atros File

--- lib/htk/config_atros.rb
+++ lib/htk/config_atros.rb
@@ -1,3 +1,15 @@
-class ConfigATROS
-  # To change this template use File | Settings | File Templates.
+module Htk
+  require 'ap'
+  require 'ostruct'
+  class ConfigATROS < OpenStruct
+    FILE_NAME="config_ATROS"
+    def write
+
+      File.open(FILE_NAME,"w") do |file|
+          @table.keys.each do |key|
+            file.puts "#{key.to_s.ljust(15)}  = #{self.send(key)}"
+          end
+      end
+    end
+  end
 end
