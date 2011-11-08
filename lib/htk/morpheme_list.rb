21bf57123e8fba483a7d14cdf262a5aeac1dcbc8
vbosch <vbosch@gmail.com>
2011-11-08 16:54:31 +0100

    added morpheme list class

--- lib/htk/morpheme_list.rb
+++ lib/htk/morpheme_list.rb
@@ -1,3 +1,32 @@
-class MorphemeList
-  # To change this template use File | Settings | File Templates.
+module Htk
+  class MorphemeList
+
+   def initialize
+     @morphemes = Array.new
+   end
+
+   def add(morpheme)
+     @morphemes.push
+   end
+
+    def each(&proc)
+      @morphemes.each &proc
+    end
+
+    def write(file_name)
+      File.open(file_name,"w") do |file|
+        @morphemes.each{|morpheme| file.puts morpheme}
+      end
+    end
+
+    def MorphemeList.load(file_name)
+
+      temp=MorphemeList.new
+      File.open(file_name,"r").each_line do |line|
+           temp.add(line.split[0].trim)
+      end
+      return temp
+    end
+
+  end
 end
